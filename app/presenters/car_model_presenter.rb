030504f6223a09387811ee9e66e890dd855e1dc8
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-01-16 20:13:16 -0300

    Implement the CarModelPresenter#photo_url

--- app/presenters/car_model_presenter.rb
+++ app/presenters/car_model_presenter.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class CarModelPresenter < BasePresenter
+  include Rails.application.routes.url_helpers
+
   def initialize(car_model)
     super(car_model)
   end
@@ -9,5 +11,11 @@ class CarModelPresenter < BasePresenter
     return manufacture.name if manufacture.present?
 
     'N/A'
+  end
+
+  def photo_url
+    return rails_blob_url(photo) if photo.attached?
+
+    'https://place-hold.it/100x100'
   end
 end
